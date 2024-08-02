1be30edb53f527af016575e3dbf594230ffc5081
Ковалева Вита <vita.kovaleva@gazprombank.ru>
2024-08-02 17:42:30 +0300

    starter fix

--- src/main/java/com/example/metricsstarter/config/MetricConfig.java
+++ src/main/java/com/example/metricsstarter/config/MetricConfig.java
@@ -1,11 +1,18 @@
 package com.example.metricsstarter.config;
 
 import lombok.Getter;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.boot.autoconfigure.AutoConfiguration;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.stereotype.Component;
 
 @Getter
-@Component
+@AutoConfiguration
+@ComponentScan("com.example.metricsstarter")
+@EnableAspectJAutoProxy
 @ConfigurationProperties(prefix = "metrics")
 public class MetricConfig {
 
